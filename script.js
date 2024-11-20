d1161cc17fefd82883819d9491c1bce6edd2735d
styven <styvenn16@gmail.com>
2024-11-20 20:29:36 -0300

    Exibindo a quantidade de itens da lista

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category");
 
 //seleciona os elementos da lista
 const expenseList = document.querySelector("ul");
+const expenseQuantity = document.querySelector("aside header p span")
 //Captura o evento de input para formatar o valor
 amount.oninput = () => {
   //obter o valor atual do input e remove os caracteres não numéricos
@@ -48,6 +49,7 @@ form.onsubmit = (e) => {
   expenseAdd(newExpense);
 };
 
+//adiciona m novo item na lista
 function expenseAdd(newExpense) {
   try {
     //cria o elemento para adicionar na lista.
@@ -96,8 +98,25 @@ function expenseAdd(newExpense) {
 
     //adiciona o item na lista
     expenseList.append(expenseItem);
+
+    //Atualiza os totais 
+    updateTotals()
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
+  }
+}
+
+//atualizar os totais
+function updateTotals(){
+  try {
+    //Recuperar todos os itens (li) da lista (ul)
+    const items = expenseList.children
+    
+    //atualiza a quantidade de itens da lista
+    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? " despesas":"despesa"}`
+  } catch (error) {
+    alert("Não foi passível atualizar o total")
+    console.log(error)
   }
 }
